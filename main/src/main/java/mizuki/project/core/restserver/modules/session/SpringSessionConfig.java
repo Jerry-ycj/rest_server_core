fb05cf4d07b96ec92dd9b02ff8ad527360147acb
yingchaojie <yingchaojie@163.com>
2017-03-25 17:01:53 +0800

    update

--- main/src/main/java/mizuki/project/core/restserver/modules/session/SpringSessionConfig.java
+++ main/src/main/java/mizuki/project/core/restserver/modules/session/SpringSessionConfig.java
@@ -1,7 +1,10 @@
 package mizuki.project.core.restserver.modules.session;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by ycj on 2017/3/22.
@@ -9,5 +12,15 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  */
 @EnableRedisHttpSession
 public class SpringSessionConfig {
+
+    @Autowired
+    private RedisOperationsSessionRepository sessionRepository;
+
+    @PostConstruct
+    private void init(){
+        // 设置session expire时间（秒）
+        sessionRepository.setDefaultMaxInactiveInterval(60*60);
+    }
+
 
 }
