a6f6be9981759d6b02f902ec9644a06d23f209c3
yingchaojie <yingchaojie@163.com>
2017-04-17 23:44:10 +0800

    bug

--- main/src/main/java/mizuki/project/core/restserver/interceptor/DefaultIntercep.java
+++ main/src/main/java/mizuki/project/core/restserver/interceptor/DefaultIntercep.java
@@ -1,6 +1,5 @@
 package mizuki.project.core.restserver.interceptor;
 
-import com.sun.deploy.net.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import mizuki.project.core.restserver.config.WebConfBean;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.time.LocalTime;
 import java.util.Enumeration;
 
