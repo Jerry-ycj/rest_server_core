b68e31dce758841df815c55cecd5b343aa59a2e5
yingchaojie <yingchaojie@163.com>
2017-12-02 15:18:08 +0800

    update

---
+++ main/src/main/java/mizuki/project/core/restserver/mod_user/bean/ret/UserRet.java
@@ -0,0 +1,31 @@
+package mizuki.project.core.restserver.mod_user.bean.ret;
+
+import mizuki.project.core.restserver.config.BasicRet;
+import mizuki.project.core.restserver.mod_user.bean.User;
+
+public class UserRet extends BasicRet{
+    private UserRetData data = new UserRetData();
+
+    public UserRetData getData() {
+        return data;
+    }
+
+    public UserRet setData(UserRetData data) {
+        this.data = data;
+        return this;
+    }
+
+    public class UserRetData{
+        private User user;
+
+        public User getUser() {
+            return user;
+        }
+
+        public UserRetData setUser(User user) {
+            this.user = user;
+            return this;
+        }
+    }
+
+}
