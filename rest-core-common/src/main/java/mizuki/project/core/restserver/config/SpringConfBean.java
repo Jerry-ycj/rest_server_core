c76a500f3a2ef91164bf678d08aa71d4404fe915
yingchaojie <yingchaojie@163.com>
2018-11-16 10:12:52 +0800

    fix bug

--- rest-core-common/src/main/java/mizuki/project/core/restserver/config/SpringConfBean.java
+++ rest-core-common/src/main/java/mizuki/project/core/restserver/config/SpringConfBean.java
@@ -7,13 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Created by ycj on 2017/2/11.
  * 对应 application.yml中spring的配置
  */
-@ConfigurationProperties("spring")
+//@ConfigurationProperties("spring")
 public class SpringConfBean {
 
     public static final String PROFILE_PRO = "pro";
     public static final String PROFILE_DEV = "dev";
 
-    // 不用那个active
+    @Value("${spring.profiles.active}")
     private String profiles;
     @Value("${var.server}")
     private String ip;
